7b0fcc2324b44b169760d2fb90c0190a35e05270
LEPERS_NICOLAS\nicol <nicolaslepers176@gmail.com>
2022-11-21 20:16:42 +0100

    J'ai fini la boite de mun

--- Assets/Scripts/Ammo.cs
+++ Assets/Scripts/Ammo.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class Ammo : MonoBehaviour
 {
     private PlayerController player;
+    public List<GameObject> SpawnObject;
+    private float _timerSwitchPlace;
+    public float TimerForSwitchPlace;
     void Start()
     {
         player = FindObjectOfType<PlayerController>();
+        SwitchPlace();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _timerSwitchPlace += Time.deltaTime;
+
+        if (_timerSwitchPlace < TimerForSwitchPlace)
+            return;
+        _timerSwitchPlace = 0;
+        SwitchPlace();
+    }
+
+    void SwitchPlace()
+    {
+        int place = Random.Range(0, SpawnObject.Count);
+        transform.position = SpawnObject[place].transform.position;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
