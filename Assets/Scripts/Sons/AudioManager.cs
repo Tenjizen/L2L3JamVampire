4e6fe4495d441b43fecdea65b6f2439c70511252
rhumyo <91311123+rhumyo@users.noreply.github.com>
2022-11-22 15:04:21 +0100

    on commence le son

--- Assets/Scripts/Sons/AudioManager.cs
+++ Assets/Scripts/Sons/AudioManager.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioClip[] Clips;
+    public static AudioManager Instance;
+    private object _coroutine;
+
+    public float VolumeMusic { get; private set; }
+
+    AudioClip GetClip(string name)
     {
-        
+        foreach (var item in Clips)
+        {
+            if (item.name == name)
+                return item;
+        }
+        return null;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayMusic(string name)
     {
-        
+        AudioClip clip = GetClip(name);
+
+        if (clip != null)
+        {
+            GameObject go = new GameObject();
+            go.name = name;
+            AudioSource source = go.AddComponent<AudioSource>();
+            source.clip = clip;
+            source.volume = VolumeMusic;
+            source.loop = true;
+            source.Play();
+        }
+    }
+
+    public void PlaySound(string name)
+    {
+        AudioClip clip = GetClip(name);
+
+        if (clip != null)
+        {
+            GameObject go = new GameObject();
+            go.name = name;
+            AudioSource source = go.AddComponent<AudioSource>();
+            source.clip = clip;
+            source.volume = VolumeMusic;
+            source.Play();
+            GameObject.Destroy(go, source.clip.length);
+        }
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+
+        PlayMusic("MainTheme_JetSetRadio");
+    }
+
+    IEnumerator PlayFootSteps(string soundName)
+    {
+        while(enabled)
+        {
+            AudioManager.Instance.PlayFootSteps("stepdirt_1");
+            yield return new WaitForSeconds(0.3f);
+        }
+
     }
 }
