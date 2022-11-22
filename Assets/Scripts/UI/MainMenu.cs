88ae48b69717da8f98ecff91e1271e8bd344c5f8
rhumyo <outteryck.romy@gmail.com>
2022-11-22 22:30:38 +0100

    menu début numéro 2

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Tkt ça marche je te jure");
+        Application.Quit();
+    }
+
+}
